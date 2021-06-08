abad6578582a799304440a3235f04b1aa0c8931a
heartsuit <nxq0108@126.com>
2021-06-08 11:58:02 +0800

    initial commit empty

---
+++ src/main/java/com/heartsuit/db2word/Db2wordApplication.java
@@ -0,0 +1,13 @@
+package com.heartsuit.db2word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2wordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2wordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heartsuit/db2word/Db2wordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartsuit.db2word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2wordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
